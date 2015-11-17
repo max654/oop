29124604eef5ec0c17005ec5bc46794424e74b0b
max654 <maxy_654@hotmail.com>
2015-11-17 21:32:50 +0500

    video 4

--- core/init.php
+++ core/init.php
@@ -1,4 +1,26 @@
 <?php
+session_start();
 
+$GLOBALS['config'] = array(
+	'mysql' => array(
+		'host' => '127.0.0.1',
+		'username' => 'root',
+		'password' => '',
+		'db' => 'oop'
+	),
+	'remember' => array(
+		'cookie_name' => 'hash',
+		'host' => 604800,
+	),
+	'session' => array(
+		'session_name' => 'user',
+	)
+);
+
+spl_autoload_register(function($class){
+	require_once 'classes/'.$class.'.php';
+});
+
+require_once 'functions/sanitize.php';
 
 ?>
